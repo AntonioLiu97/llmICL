b949e3418c089cdc625c7edae7d6068d481bf4bd
Toni Liu <jl3499@cornell.edu>
2023-12-23 00:55:55 -0500

    add discrete BT distance

--- models/ICL.py
+++ models/ICL.py
@@ -340,19 +340,22 @@ class MultiResolutionPDF:
         assert np.allclose(self.bin_center_arr[1:] - self.bin_width_arr[1:]/2, 
                            self.bin_center_arr[:-1] + self.bin_width_arr[:-1]/2), "bin overlap detected"
         
-    def discretize(self, func, mode = 'bin_center'):
+    def discretize(self, func, mode = 'pdf'):
         """
         Args:
             func: a function supported on self.bin_center_arr.
                   should be implmented using numpy operations for parallelization
-            mode: 'bin_center': approximate probability of bin using its center
-                  'intgt': integrate over bin using scipy quad
+            mode: 'pdf': approximate probability of bin using its center
+                  'cdf': integrate over bin 
         Populate bin height by dicretizng
         """
-        if mode == 'bin_center':
+        if mode == 'pdf':
             self.bin_height_arr = func(self.bin_center_arr)
-        elif mode == 'intgt':
-            raise NotImplementedError 
+        elif mode == 'cdf':
+            right_edge = self.bin_center_arr + self.bin_width_arr/2
+            left_edge = self.bin_center_arr - self.bin_width_arr/2
+            prob_arr = func(right_edge) - func(left_edge)
+            self.bin_height_arr = prob_arr / self.bin_center_arr
         self.normalize()
         
     def BT_dist(self, Multi_PDF):
@@ -366,6 +369,7 @@ class MultiResolutionPDF:
     def KL_div(self, Multi_PDF):
         """
         Calculate the KL divergence D_KL(self||Multi_PDF)
+        Prone to numerical instabilities
         """          
         assert np.all(self.bin_center_arr == Multi_PDF.bin_center_arr), "Only PDFs of the same discretization are comparable"
         log_ratio = np.log(self.bin_height_arr) - np.log(Multi_PDF.bin_height_arr)
